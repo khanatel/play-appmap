286c1c2adf44ddb3020aa09519921f48596dc7df
Khanate L <40157668+khanatel@users.noreply.github.com>
2023-06-29 14:43:02 +0700

    Update purchases.js

--- src/routes/purchases.js
+++ src/routes/purchases.js
@@ -3,13 +3,10 @@ const router = express.Router()
 
 const  { 
     getPurchases,
-    getPurchase,
     createPurchases
 } = require('../controllers/purchases.js')
 
 router.get('/', getPurchases)
-
-router.get('/:purchasesID', getPurchase)
 
 router.post('/', createPurchases) 
 
